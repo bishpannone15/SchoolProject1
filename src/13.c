535e0d48b97c7175f19ece890da8ba38c0685ad9
bishpannone15 <bishpannone15@gmail.com>
2025-03-18 05:16:30 +0000

    Add 13.c

---
+++ src/13.c
@@ -0,0 +1,8 @@
+int main() {
+    int i = 42;
+    if (i > 0) {
+        printf("The answer to life, the universe, and everything is %d\n", i);
+    } else {
+        printf("The answer to life, the universe, and everything is %d\n", -i);
+    }
+}
