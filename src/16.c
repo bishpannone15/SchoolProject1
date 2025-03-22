874930479d1987f9f84648a1288a0ed710d31a26
bishpannone15 <bishpannone15@gmail.com>
2025-03-22 16:00:17 +0000

    Add 16.c

---
+++ src/16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            printf("* ");
+        } else {
+            printf("  ");
+        }
+    }
+    
+    printf("\n");
+    return 0;
+}
