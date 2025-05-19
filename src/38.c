d3ee330dc4c42916053f49b23ef1b7d165971a1f
bishpannone15 <bishpannone15@gmail.com>
2025-05-19 23:55:20 +0000

    Add 38.c

---
+++ src/38.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for(i = 0; i < 5; i++) {
+        printf("%d", i);
+        if (i != 4) {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
